67bd8be3773d91138088ea76c2471b14e2ebd449
Hugo Frappier <hugo@cekoya.com>
2015-12-13 20:44:19 -0500

    Chapter 6

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,6 +1,10 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
+  import Phoenix.Controller
   import Comeonin.Bcrypt, only: [checkpw: 2]
+  alias Rumbl.Router.Helpers
+
+  ### Plug
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -11,6 +15,8 @@ defmodule Rumbl.Auth do
     user = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
   end
+
+  ### Session management
 
   def login(conn, user) do
     conn
@@ -36,5 +42,18 @@ defmodule Rumbl.Auth do
   def logout(conn) do
     conn
     |> delete_session(:user_id)
+  end
+
+  ### Authentication filter
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
--- web/web.ex
+++ web/web.ex
@@ -34,6 +34,7 @@ defmodule Rumbl.Web do
       import Ecto.Query, only: [from: 1, from: 2]
 
       import Rumbl.Router.Helpers
+      import Rumbl.Auth, only: [authenticate_user: 2]
     end
   end
 
@@ -54,6 +55,8 @@ defmodule Rumbl.Web do
   def router do
     quote do
       use Phoenix.Router
+
+      import Rumbl.Auth, only: [authenticate_user: 2]
     end
   end
 
