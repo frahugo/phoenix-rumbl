1d9a80e25aa05d2cd1d42a7b17c4abaaa285aad0
Hugo Frappier <hugo@cekoya.com>
2015-12-15 17:50:14 -0500

    Add some tests

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -12,8 +12,23 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(Rumbl.User, user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+      user = user_id && repo.get(Rumbl.User, user_id) ->
+        put_current_user(conn, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   ### Session management
